ddf6ab22474f39711938a77deaa1e0b08344b137
Fabian Meiswinkel <fabianm@microsoft.com>
2019-05-02 09:12:07 +0200

    Add project files.

---
+++ GetMinRU/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GetMinRU
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
